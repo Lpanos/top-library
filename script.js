6af4298f81770fa423fc92bf02523652d92830c8
LandonPanos <28903662+Lpanos@users.noreply.github.com>
2024-04-30 21:13:14 -0700

    change factory function to class

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 const myLibrary = [];
 
-
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function(){
-        return this.title + ', by ' + this.author + ', ' + this.pages + ' pages, ' + this.read;
-    };
-}
+class Book {
+    constructor (title,author,pages,read){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+};
 
 function addBookToLibrary(title, author, pages, read){
     const newBook = new Book(title, author, pages, read)
